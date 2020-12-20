af804d5327051c41b9db323c9339e0cd69d23b33
ashraf <ashraf1abdelrasool@gmail.com>
2020-12-20 21:34:55 +0200

    Initial commit

--- src/test/java/org/revo/ufmi/UfmiApplicationTests.java
+++ src/test/java/org/revo/ufmi/UfmiApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class UfmiApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
